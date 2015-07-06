bc27e257ac10bb3a8afdf3bef435b205777bffe5
Ghadi Shayban <gshayban@gmail.com>
2015-07-06 19:30:27 -0400

    initial

---
+++ test/com/champbacon/pex_test.clj
@@ -0,0 +1,7 @@
+(ns com.champbacon.pex-test
+  (:require [clojure.test :refer :all]
+            [com.champbacon.pex :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
