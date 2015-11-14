f72e2558c4bc20a8a345b3e8554c8587da2c944f
Ghadi Shayban <gshayban@gmail.com>
2015-11-13 22:54:24 -0500

    bump clojure

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "http://github.com/ghadishayban/pex"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0-beta2"]]
+  :dependencies [[org.clojure/clojure "1.8.0-RC1"]]
   :java-source-paths ["src-java"])
