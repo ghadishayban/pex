0ea26fdc1428dc4618c3548a7760565fcdea32ed
Ghadi Shayban <gshayban@gmail.com>
2015-11-16 20:46:39 -0500

    clojure 1.7

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "http://github.com/ghadishayban/pex"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0-RC1"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :java-source-paths ["src-java"])
